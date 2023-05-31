8de0054f8952af9a69066e44cfc939f846c748a9
Eduardo-Antoniassi-Lobato <>
2023-05-31 16:41:58 +0000

    Add models structure to database

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["think-therefore-blog.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["think-therefore-blog.herokuapp.com",
+                 os.environ.get("localhost")]
 
 
 # Application definition
